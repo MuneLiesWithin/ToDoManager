7679535dac050b21ec159a5ee8080b2af46c7188
MuneLiesWithin <romulo_medeiros@hotmail.com>
2023-08-02 13:49:18 -0300

    initial

---
+++ src/main/java/com/mune/todomanager/todo/Todo.java
@@ -0,0 +1,80 @@
+package com.mune.todomanager.todo;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.validation.constraints.Size;
+
+@Entity
+public class Todo {
+	
+	@Id
+	@GeneratedValue
+	private int id;
+	private String username;
+	@Size(min=5, message="Enter at least 5 characters")
+	private String description;
+	private LocalDate localDate;
+	private boolean done;
+	
+	public Todo() {
+		
+	}
+	
+	public Todo(int id, String username, String description, LocalDate localDate, boolean done) {
+		super();
+		this.id = id;
+		this.username = username;
+		this.description = description;
+		this.localDate = localDate;
+		this.done = done;
+	}
+	
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getUsername() {
+		return username;
+	}
+
+	public void setUsername(String username) {
+		this.username = username;
+	}
+
+	public String getDescription() {
+		return description;
+	}
+
+	public void setDescription(String description) {
+		this.description = description;
+	}
+
+	public LocalDate getLocalDate() {
+		return localDate;
+	}
+
+	public void setLocalDate(LocalDate localDate) {
+		this.localDate = localDate;
+	}
+
+	public boolean getDone() {
+		return done;
+	}
+
+	public void setDone(boolean done) {
+		this.done = done;
+	}
+
+	@Override
+	public String toString() {
+		return "Todo [id=" + id + ", username=" + username + ", description=" + description + ", localDate=" + localDate
+				+ ", done=" + done + "]";
+	}
+}
---
+++ src/main/resources/data.sql
@@ -0,0 +1,11 @@
+insert into todo (ID, USERNAME, DESCRIPTION, LOCAL_DATE, DONE)
+values (10001, 'Maria', 'Get AWS Certified', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, LOCAL_DATE, DONE)
+values (10002, 'Maria', 'Learn Google Cloud', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, LOCAL_DATE, DONE)
+values (10003, 'José', 'Learn React', CURRENT_DATE(), false);
+
+insert into todo (ID, USERNAME, DESCRIPTION, LOCAL_DATE, DONE)
+values (10004, 'José', 'Learn Node.js', CURRENT_DATE(), false);
